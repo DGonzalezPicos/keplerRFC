36ddd9acd9c46bafeee7289141e1f3ac0fb70639
MiNotebook <dariogonzalezpicos@gmail.com>
2020-11-12 17:59:00 +0000

    Added Grid and Correlation Parameters

--- RFCfun.py
+++ RFCfun.py
@@ -49,8 +49,65 @@ def RFC(file, feat):
     
     ax.set_title("Random Forest Feature Importances (MDI)")
     plt.show()
-    return forest_model
+    return train_X, val_X, train_y, val_y, forest_model
     
-file = "./keplerRFC/cumulative.csv"
+file = "./cumulative.csv"
 feat = ['koi_prad', 'koi_period', 'koi_impact', 'koi_duration']
-RFC(file, feat)
+train_X, val_X, train_y, val_y, rfcmodel = RFC(file, feat)
+
+
+#%%
+# =============================================================================
+#                   Parameter selection with GridSearchCV
+# =============================================================================
+import datetime
+begin_time = datetime.datetime.now()
+
+from sklearn.model_selection import GridSearchCV
+
+parameters = {'n_estimators': np.arange(100,601,100), 
+              'max_features': [None, 'auto','sqrt','log2'],
+              'max_depth': [6,10,12]}
+clf = GridSearchCV(rfcmodel, parameters)
+clf.fit(train_X,train_y)            
+print('Best parameters... ',clf.best_params_)
+
+
+print('Execution time...', datetime.datetime.now() - begin_time)
+#%%
+# =============================================================================
+#              Correlation between numerical features in the dataset
+# =============================================================================
+import seaborn as sns
+corr_df=X[feat]  #New dataframe to calculate correlation between numeric features
+cor= corr_df.corr(method='pearson')
+print(cor)
+print('----------------------------------------------------------------------')
+correlated = []
+for i in feat:
+    for j in feat:
+        eps = cor[i][j]
+        if i != j:
+            if eps > 0.6:   # arbitrary threshold
+                if ([j,i] in correlated)==True: # avoid adding the same pair twice
+                    None
+                else:
+                        correlated.append([i,j])
+                        print('The features {:s} and {:s} are highly correlated. R2 = {:.2f}'.format(i,j, eps))
+#%%            
+fig, ax =plt.subplots(figsize=(8, 6))
+plt.title("Correlation Plot")
+
+sns.heatmap(cor, mask=np.zeros_like(cor, dtype=np.bool), 
+            cmap=sns.diverging_palette(220, 10, as_cmap=True),
+            square=True, ax=ax)
+
+plt.savefig('../Images/pearson.png', dpi=150)
+plt.show()
+# there is significant correlation between koi_impact and koi_prad as expected
+''' It seems that multicollinearity is well handled by RFC, however, it will make
+it easier and faster for the user to eliminate highly correlated variables,
+with pearson > 0.7 for instance '''
+
+
+
